2e7f91f54500e387dcb1d9486e26b53861c31bb6
ninjigen <ninjigen@gmail.com>
2015-11-01 16:43:46 +0100

    upgrading Test-TDDModule
    Name Change, Test failed color changed

--- Test-TDDModule.ps1
+++ Test-TDDModule.ps1
@@ -11,15 +11,15 @@ process{
             $ModuleName = $_.BaseName
             $ModulePath = $_.FullName
             Import-Module $ModulePath -Force
-            $CommandList = Get-Command -Module $ModuleName -CommandType Function | Where-Object {$_.Verb -eq 'Test'}
+            $CommandList = Get-Command -Module $ModuleName -CommandType Function | Where-Object {$_.Verb -eq 'Test'} | Sort-Object
             $CommandList | Foreach-Object {
                 $CommandDefinition = $_.Definition
                 if(Invoke-Expression -Command $_.Name) {
                     Write-Host ("{0} - PASSED TEST - {1}" -f (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss'),$_.Name) -Foreground Green
                 } else {
                     $Result = $False
-                    Write-Host ("{0} - FAILED TEST - {1}" -f (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss'),$_.Name) -Foreground Red
-                    Write-Host ("{0} - FAILED TEST - Content : {1}" -f (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss'),$CommandDefinition) -Foreground Red
+                    Write-Host ("{0} - FAILED TEST - {1}" -f (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss'),$_.Name) -Foreground Magenta
+                    Write-Host ("{0} - FAILED TEST - Content : {1}" -f (Get-Date -Format 'yyyy/MM/dd-HH:mm:ss'),$CommandDefinition) -Foreground Magenta
                 }
             }
         }
